5597fc75816c24a573d50d81bad055233caf7657
Noel <23-08992@g.batstate-u.edu.ph>
2024-06-21 19:20:14 +0800

    installed raygui and removed wxWdigets

--- main.cpp
+++ main.cpp
@@ -1,71 +1,26 @@
-#include <wx/wx.h>
-#include <fmt/format.h>
+#include <fmt/core.h>
+#include <raylib.h>
 
 
-class MyApp : public wxApp{
-    public:
-        bool OnInit() override;
-};
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-wxIMPLEMENT_APP(MyApp);
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-class MyFrame : public wxFrame{
-    public:
-        MyFrame();
-    private:
-        void OnHello(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
-};
+    SetTargetFPS(60);
 
-bool MyApp::OnInit(){
-    wxFrame *frame = new wxFrame(NULL, wxID_ANY, "Hello World");
-    frame->Show(true);
-    return true;
-}
+    while (!WindowShouldClose()) {
+        BeginDrawing();
 
-enum{
-    ID_Hello = 1
-};
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
-MyFrame::MyFrame()
-        : wxFrame(nullptr, wxID_ANY, "Hello World")
-{
-    wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl+H",
-                     "Help string shown in status bar for this menu item");
-    menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
- 
-    wxMenu *menuHelp = new wxMenu;
-    menuHelp->Append(wxID_ABOUT);
- 
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
-    menuBar->Append(menuHelp, "&Help");
- 
-    SetMenuBar(menuBar);
- 
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
- 
-    Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-    Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
-}
+        EndDrawing();
+    }
 
-void MyFrame::OnExit(wxCommandEvent& event)
-{
-    Close(true);
-}
+    CloseWindow();
 
-void MyFrame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
-}
 
-void MyFrame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello world from wxWidgets!");
+    return 0;
 }
