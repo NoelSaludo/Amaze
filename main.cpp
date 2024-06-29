1b8ce93e3b0d4fce1e06696ec770b613f2ae4adb
Noel <23-08992@g.batstate-u.edu.ph>
2024-06-29 10:24:18 +0800

    removed usage of raylib, raygui, fmt

--- main.cpp
+++ main.cpp
@@ -1,40 +1,7 @@
-#define RAYGUI_IMPLEMENTATION
 #include <iostream>
-#include <fmt/core.h>
-#include <raylib.h>
-#include "raygui.h"
 
-
-int main() {
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-    SetTargetFPS(60);
-
-    bool showMessageBox = false;
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        
-        ClearBackground(RAYWHITE);
-        if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
-
-            if (showMessageBox)
-            {
-                int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
-
-                if (result >= 0) showMessageBox = false;
-            }
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-
+int main(int argc, char const *argv[])
+{
+    
     return 0;
 }
