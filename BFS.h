ba711ef64ce7ea2c1397f9f46b1b750913659681
Noel <23-08992@g.batstate-u.edu.ph>
2024-07-08 10:48:12 +0800

    moved DnP struct to BFS

--- BFS.h
+++ BFS.h
@@ -2,10 +2,13 @@
 #include <string>
 #include <queue>
 #include "graph.h"
-#include "Dijsktra.h"
 
 #ifndef BFS_H_
 #define BFS_H_
+struct DnP{
+    std::vector<int> depth;
+    std::vector<int> previous;
+};
 
 void BFS(const Graph &);
 DnP BFSUtil(const Graph &,const int , std::vector<bool> &, std::queue<int> &);
